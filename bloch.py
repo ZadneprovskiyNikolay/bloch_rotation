5594273747ae67466ad9b32f63067204fdcd0f17
Nikolay Zadneprovskiy <zadneprovskiywork@gmail.com>
2023-05-17 16:12:02 +0300

    feat: visualize rotation path properly

--- bloch.py
+++ bloch.py
@@ -1,6 +1,7 @@
 import numpy as np
 from cmath import phase
 from math import cos, sin, acos, e
+from utils import normalize_vector
 
 def state_to_bloch(state):    
     theta = 2*acos(abs(state[0]))
@@ -17,4 +18,23 @@ def xyz_from_bloch(theta, phi):
     x = cos(phi) * sin(theta)
     y = sin(phi) * sin(theta)
     z = cos(theta)
-    return x, y, z
+    return np.array([x, y, z])
+
+def rotation_trace_points(n, A, B, alpha, total_points=50):
+    # Создаем ортонормированные базисные векторы i и j в плоскости вращения.
+    # при этом i указывает на дугу вращения и делит ее пополам.
+    R = np.linalg.norm((B - A)) / (2*sin(abs(alpha)/2))
+    j = normalize_vector(np.array(B) - np.array(A))
+    if alpha > 0:
+        i = np.cross(j, n)
+        r0 = B - R*(cos(alpha/2)*i + sin(alpha/2)*j)
+    else:
+        i = np.cross(n, j)
+        r0 = A - R*(cos(alpha/2)*i + sin(alpha/2)*j)
+
+    circle_point = lambda i, j, r0, R, phi: r0 + R*cos(phi)*i + R*sin(phi)*j
+    angles = list(np.linspace(-alpha/2, 0, int(total_points/2)))
+    angles += list(np.linspace(0, alpha/2, int(total_points/2)))
+    points = [circle_point(i, j, r0, R, phi) for phi in angles]
+
+    return points
---
+++ utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def normalize_vector(v):
+    return v / np.linalg.norm(v)
